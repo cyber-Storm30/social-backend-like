b1c407463606959ba6b124ee23028b39047af62e
cyber-Storm30 <babairanajit@gmail.com>
2024-04-06 12:51:31 +0530

    backing up mac

--- src/services/like.service.js
+++ src/services/like.service.js
@@ -1,6 +1,7 @@
 import PostModel from "../models/post.model.js";
 import axios from "axios";
 import { BASE_URL } from "../config/connection.js";
+import kafka from "../config/kafkaClient.js";
 
 class LikeService {
   async likePost(userId, postId) {
@@ -11,6 +12,27 @@ class LikeService {
       }
       if (!post.likes.includes(userId)) {
         await post.updateOne({ $push: { likes: userId } });
+        const producer = kafka.producer();
+
+        await producer.connect();
+
+        await producer.send({
+          topic: "notifications",
+          messages: [
+            {
+              partition: 0,
+              key: "notification-update",
+              value: JSON.stringify({
+                type: "LIKE POST",
+                postId,
+                receiverId: post.userId,
+                senderId: userId,
+              }),
+            },
+          ],
+        });
+
+        await producer.disconnect();
         return {
           success: true,
           data: "LIKED",
